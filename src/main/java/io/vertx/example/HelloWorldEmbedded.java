3e2c8a9d02d33781519cef8c632325a04dac11fd
garg-anuj <anujgargcse@gmail.com>
2017-12-12 11:48:10 +0530

    Turn back to hard coded strings

--- src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -2,26 +2,20 @@ package io.vertx.example;
 
 import io.vertx.core.Vertx;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
-      ResourceBundle res = ResourceBundle.getBundle("io.vertex.example.helloworld");
-    // Create an HTTP server which simply returns "Hello World!" to each request.
     Vertx.vertx()
       .createHttpServer()
-      .requestHandler(req -> req.response().end(res.getString("hello")))
+      .requestHandler(req -> req.response().end("Hello world"))
       .listen(8080, handler -> {
         if (handler.succeeded()) {
-          System.out.println(res.getString("localhost"));
+          System.out.println("You can now visit http://localhost:8080 in your browser");
         } else {
-          System.err.println(res.getString("failed"));
+          System.err.println("failed to start");
         }
       });
   }
